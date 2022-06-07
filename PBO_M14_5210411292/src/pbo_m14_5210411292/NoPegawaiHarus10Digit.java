d76180026ee86b552bdcb5c4828c645f411db637
K1coL <100859565+K1coL@users.noreply.github.com>
2022-06-07 17:09:24 +0700

    Add files via upload

---
+++ PBO_M14_5210411292/src/pbo_m14_5210411292/NoPegawaiHarus10Digit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_m14_5210411292;
+
+/**
+ *
+ * @author kicol
+ */
+public class NoPegawaiHarus10Digit extends Exception{
+    @Override
+    public String getMessage(){
+        return "No pegawai harus berisi 10 Digit!";
+    } 
+}
---
+++ PBO_M14_5210411292/src/pbo_m14_5210411292/NoPegawaiHarusAngka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_m14_5210411292;
+
+/**
+ *
+ * @author kicol
+ */
+public class NoPegawaiHarusAngka extends Exception{
+    @Override
+    public String getMessage(){
+        return "No pegawai harus berisi angka!";
+    } 
+}
